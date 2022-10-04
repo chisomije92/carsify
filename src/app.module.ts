3bd3affb3189b93ab4c20372eaa5722937c3d74a
Chisom Ijeomah <chisomije92@gmail.com>
2022-10-04 14:01:41 +0100

    set up mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,18 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
 
 @Module({
-  imports: [UsersModule, ReportsModule],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://Jerry:X1t5G9uJbTLaj6yW@cluster0.w1dc5.mongodb.net/carsify?authMechanism=SCRAM-SHA-1',
+    ),
+    UsersModule,
+    ReportsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
