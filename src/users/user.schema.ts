a40184f0713984e89a4929aadd88a3c04fc3db7e
Chisom Ijeomah <chisomije92@gmail.com>
2022-10-05 10:02:01 +0100

    create a user

--- src/users/user.schema.ts
+++ src/users/user.schema.ts
@@ -7,7 +7,7 @@ export type UserDocument = User & Document;
 @Schema()
 export class User {
   @Prop()
-  name: string;
+  email: string;
 
   @Prop()
   password: string;
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,14 @@
+/* eslint-disable prettier/prettier */
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User, UserDocument } from './user.schema';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+  async create(email: string, password: string) {
+    return await this.userModel.create({ email, password });
+    //return user;
+  }
+}
