e8d5ff8723c5fb76f74702eb27286b75edde9049
Chisom Ijeomah <chisomije92@gmail.com>
2022-10-05 18:09:10 +0100

    set up cookie session

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,16 @@ import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
+import cookieSession = require('cookie-session');
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.use(
+    cookieSession({
+      keys: ['xyazbxbx'],
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
