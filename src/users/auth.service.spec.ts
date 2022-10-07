8c9bfb71ddca32cbeeca295e3fc3e7b5b11ef7de
Chisom Ijeomah <chisomije92@gmail.com>
2022-10-07 12:06:18 +0100

    improve test fil layout

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -3,26 +3,31 @@ import { Test } from '@nestjs/testing';
 import { AuthService } from './auth.service';
 import { UsersService } from './users.service';
 
-it('can create an instance of auth service', async () => {
-  const fakeUsersService: Partial<UsersService> = {
-    find: jest.fn().mockImplementation(() => Promise.resolve([])),
-    create: jest
-      .fn()
-      .mockImplementation((email: string, password: string) =>
-        Promise.resolve({ _id: 1, email, password }),
-      ),
-  };
-  const module = await Test.createTestingModule({
-    providers: [
-      AuthService,
-      {
-        provide: UsersService,
-        useValue: fakeUsersService,
-      },
-    ],
-  }).compile();
+describe('AuthService', () => {
+  let service: AuthService;
+  beforeEach(async () => {
+    const fakeUsersService: Partial<UsersService> = {
+      find: jest.fn().mockImplementation(() => Promise.resolve([])),
+      create: jest
+        .fn()
+        .mockImplementation((email: string, password: string) =>
+          Promise.resolve({ _id: 1, email, password }),
+        ),
+    };
+    const module = await Test.createTestingModule({
+      providers: [
+        AuthService,
+        {
+          provide: UsersService,
+          useValue: fakeUsersService,
+        },
+      ],
+    }).compile();
 
-  const service = module.get(AuthService);
+    service = module.get(AuthService);
+  });
 
-  expect(service).toBeDefined();
+  it('can create an instance of auth service', async () => {
+    expect(service).toBeDefined();
+  });
 });
