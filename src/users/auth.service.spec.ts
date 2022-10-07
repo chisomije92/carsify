6d7eb2c812f3d3d518956ae21270c971763463e6
Chisom Ijeomah <chisomije92@gmail.com>
2022-10-07 12:02:46 +0100

    add type safety to fakeUsersService

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -4,10 +4,13 @@ import { AuthService } from './auth.service';
 import { UsersService } from './users.service';
 
 it('can create an instance of auth service', async () => {
-  const fakeUsersService = {
-    find: () => Promise.resolve([]),
-    create: (email: string, password: string) =>
-      Promise.resolve({ _id: 1, email, password }),
+  const fakeUsersService: Partial<UsersService> = {
+    find: jest.fn().mockImplementation(() => Promise.resolve([])),
+    create: jest
+      .fn()
+      .mockImplementation((email: string, password: string) =>
+        Promise.resolve({ _id: 1, email, password }),
+      ),
   };
   const module = await Test.createTestingModule({
     providers: [
