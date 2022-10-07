35d3340e644da0843cd6ea37c98924f171a182eb
Chisom Ijeomah <chisomije92@gmail.com>
2022-10-07 19:15:32 +0100

    revise security issue

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,7 +7,6 @@ import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
 import { APP_PIPE } from '@nestjs/core';
 import cookieSession = require('cookie-session');
-import { url } from 'inspector';
 
 @Module({
   imports: [
@@ -15,9 +14,7 @@ import { url } from 'inspector';
       isGlobal: true,
       envFilePath: `.env.${process.env.NODE_ENV}`,
     }),
-    //MongooseModule.forRoot(
-    //  `mongodb+srv://Jerry:Ee6vM0mUe1uihgFR@cluster0.w1dc5.mongodb.net/carsify?authMechanism=SCRAM-SHA-1`,
-    //),
+
     MongooseModule.forRootAsync({
       imports: [ConfigModule],
       useFactory: (config: ConfigService) => {
