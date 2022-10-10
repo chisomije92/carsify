34dcd9d3ac84594053fcb362ac2ebcf8f6397f58
Chisom Ijeomah <chisomije92@gmail.com>
2022-10-10 16:16:34 +0100

    implement creating a report

--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { CreateReportDto } from './dtos/create-report.dto';
+import { Report, ReportDocument } from './report.schema';
 
 @Injectable()
-export class ReportsService {}
+export class ReportsService {
+  constructor(
+    @InjectModel(Report.name) private reportModel: Model<ReportDocument>,
+  ) {}
+
+  create(reportDto: CreateReportDto) {
+    const report = this.reportModel.create(reportDto);
+    return report;
+  }
+}
