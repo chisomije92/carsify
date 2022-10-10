744d438a2446bcc1d1d3e00f26569c5b7e31dd37
Chisom Ijeomah <chisomije92@gmail.com>
2022-10-10 15:43:34 +0100

    add properties to reports schema

--- src/reports/report.schema.ts
+++ src/reports/report.schema.ts
@@ -1,14 +1,37 @@
 /* eslint-disable prettier/prettier */
 
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { Document } from 'mongoose';
+import { Document, Types } from 'mongoose';
 
 export type ReportDocument = Report & Document;
 
 @Schema()
 export class Report {
+  _id: Types.ObjectId;
+
   @Prop()
-  price: string;
+  approved: boolean;
+
+  @Prop()
+  price: number;
+
+  @Prop()
+  make: string;
+
+  @Prop()
+  model: string;
+
+  @Prop()
+  year: number;
+
+  @Prop()
+  lng: number;
+
+  @Prop()
+  lat: number;
+
+  @Prop()
+  mileage: number;
 }
 
 export const ReportSchema = SchemaFactory.createForClass(Report);
